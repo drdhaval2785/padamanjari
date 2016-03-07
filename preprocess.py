369f1994900e225cc806ad320ef685cec0ce3cf6
drdhaval2785 <drdhaval2785@gmail.com>
2016-03-07 18:03:10 +0530

    output in output folder

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,8 @@
 # This Python file uses the following encoding: utf-8
-import re,codecs
+import re,codecs,sys
 import transcoder
 def changelist(input):
-	changelist = [(u'\ufeff ',u''),(u'\ufeff',u''),(u'\u2028',u'#2%'),(u'\xd8',u'#4%'),(u'\xc3',u'#5%'),(u'\xc2',u''),] # 
+	changelist = [(u'\ufeff ',u''),(u'\ufeff',u''),(u'\u2028',u'#2%'),(u'\xd8',u'#4%'),(u'\xc3',u''),(u'\xc2',u''),] # 
 	regexlist = [(r'([#][2][%])i([a-zA-Z0-9 ."~/|]+[>])','\g<1><\g<2>')]
 	for (a,b) in changelist:
 		input = input.replace(a,b)
@@ -45,13 +45,16 @@ def alterations(filein,fileout):
 	log.close()
 	print 'changing to slp1'
 	output = transcoder.transcoder_processString(data,'vel','slp1')
-	fout = codecs.open(fileout,'w','utf-8')
-	fout.write(output)
-	fout.close()
+	#fout1 = codecs.open(fileout,'w','utf-8')
+	#fout1.write(output)
+	#fout1.close()
 	print 'changing to deva'
 	output = transcoder.transcoder_processString(output,'slp1','deva')
 	output = output.replace('#','')
-	fout1 = codecs.open('output1.txt','w','utf-8')
+	#output = output.replace('\n','<br/>')
+	fout1 = codecs.open(fileout,'w','utf-8')
 	fout1.write(output)
 	fout1.close()
-alterations('test.htm','output.txt')
+if __name__=="__main__":
+	filein = sys.argv[1]
+	alterations(filein,'output/'+filein[:-3]+'txt')
