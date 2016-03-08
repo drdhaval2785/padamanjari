b7245e26ccb318ad33b16ce2d7e8136bffcb21b9
drdhaval2785 <drdhaval2785@gmail.com>
2016-03-08 19:50:00 +0530

    padamanjari.txt as combined output

--- convertor.sh
+++ convertor.sh
@@ -11,3 +11,6 @@ do
 	# conversion
 	python preprocess.py 'PADAMANJARI/PADAMANJARI/'$FILENAME
 done
+echo "preparing padamanjari.txt"
+cat output/*[1234].txt > padamanjari.txt
+echo "prepared padamanjari.txt"
